b9b6ae2a6d7ec6a29b1f5e2168288fe32c9bf8c0
Jd's Code Lab <jdscodelab@gmail.com>
2021-04-07 13:36:31 +0530

    Add files via upload
    Initial Commit.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColorModes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BlackMode_Checked(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.ColorMode = "Black";
+
+            //and to save the settings
+            Properties.Settings.Default.Save();
+        }
+
+        private void DarkMode_Checked(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.ColorMode = "Dark";
+
+            //and to save the settings
+            Properties.Settings.Default.Save();
+        }
+
+        private void LightkMode_Checked(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.ColorMode = "Light";
+
+            //and to save the settings
+            Properties.Settings.Default.Save();
+        }
+
+        private void WhiteMode_Checked(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.ColorMode = "White";
+
+            //and to save the settings
+            Properties.Settings.Default.Save();
+        }
+    }
+}
